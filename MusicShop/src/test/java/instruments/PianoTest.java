793d7dfd30df05f88cf537d6c1bccde09f2ab546
Anita Safar <ancsa0416@gmail.com>
2022-01-14 16:48:01 +0000

    hasMaterial test added to PianoTest

--- MusicShop/src/test/java/instruments/PianoTest.java
+++ MusicShop/src/test/java/instruments/PianoTest.java
@@ -1,6 +1,9 @@
 package instruments;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PianoTest {
 
@@ -10,5 +13,9 @@ public class PianoTest {
     public void before() {
         piano = new Piano(Material.WOOD, Type.KEYBOARD, 199, 425, 88, 3);
     }
-    
+
+    @Test
+    public void hasMaterial() {
+        assertEquals(Material.WOOD, piano.getMaterial());
+    }
 }
