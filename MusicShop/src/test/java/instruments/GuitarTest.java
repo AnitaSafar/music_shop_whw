1a4885799799ac10db43753402186840870410e9
Anita Safar <ancsa0416@gmail.com>
2022-01-14 14:54:37 +0000

    GuitarTest setup done

--- MusicShop/src/test/java/instruments/GuitarTest.java
+++ MusicShop/src/test/java/instruments/GuitarTest.java
@@ -1,4 +1,14 @@
 package instruments;
 
+import org.junit.Before;
+
 public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before() {
+        guitar = new Guitar("Wood", "String", 5, 10.50, 6);
+    }
+
 }
