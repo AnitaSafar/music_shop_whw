de2066066009279802b1753da9896a0082c9f3a1
Anita Safar <ancsa0416@gmail.com>
2022-01-14 15:20:54 +0000

    Instrument setup done

--- MusicShop/src/main/java/instruments/Instrument.java
+++ MusicShop/src/main/java/instruments/Instrument.java
@@ -1,4 +1,16 @@
 package instruments;
 
 public abstract class Instrument {
+
+    private String material;
+    private String type;
+    private double buyingPrice;
+    private double sellingPrice;
+
+    public Instrument(String material, String type, double buyingPrice, double sellingPrice) {
+        this.material = material;
+        this.type = type;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+    }
 }
