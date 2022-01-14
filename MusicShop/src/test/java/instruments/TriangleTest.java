4cdc67ea7c47146d457acf8a2286ace235ed71ad
Anita Safar <ancsa0416@gmail.com>
2022-01-14 17:13:59 +0000

    hasMaterial added to TriangleTest

--- MusicShop/src/test/java/instruments/TriangleTest.java
+++ MusicShop/src/test/java/instruments/TriangleTest.java
@@ -1,6 +1,9 @@
 package instruments;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TriangleTest {
 
@@ -9,5 +12,10 @@ public class TriangleTest {
     @Before
     public void before() {
         triangle = new Triangle(Material.METAL, Type.PERCUSSION, 6, 12.40, 1);
+    }
+
+    @Test
+    public void hasMaterial() {
+        assertEquals(Material.METAL, triangle.getMaterial());
     }
 }
