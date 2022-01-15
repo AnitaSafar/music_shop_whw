4fb8d663b62a763e374ff110059e39297e53b563
Anita Safar <ancsa0416@gmail.com>
2022-01-15 13:57:26 +0000

    GuitarStringTest created, setup done

---
+++ MusicShop/src/test/java/items/GuitarStringTest.java
@@ -0,0 +1,16 @@
+package items;
+
+import org.junit.Before;
+
+public class GuitarStringTest {
+
+    GuitarString guitarString;
+
+    @Before
+    public void before() {
+        guitarString = new GuitarString("Yamaha", 5, 9.90);
+    }
+
+
+
+}
