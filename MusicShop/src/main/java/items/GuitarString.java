1502219d3f8de2517477f6cd9df1899e74ae260d
Anita Safar <ancsa0416@gmail.com>
2022-01-15 14:02:10 +0000

    GuitarString class created, setup done

---
+++ MusicShop/src/main/java/items/GuitarString.java
@@ -0,0 +1,9 @@
+package items;
+
+public class GuitarString extends Item {
+
+    public GuitarString(String description, double buyingPrice, double sellingPrice) {
+        super(description, buyingPrice, sellingPrice);
+    }
+
+}
