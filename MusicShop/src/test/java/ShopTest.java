d6b5949d6b56e2cdf3b013b98eadf801b090aad5
Anita Safar <ancsa0416@gmail.com>
2022-01-15 14:59:14 +0000

    canAddToStock added to ShopTest

--- MusicShop/src/test/java/ShopTest.java
+++ MusicShop/src/test/java/ShopTest.java
@@ -1,11 +1,26 @@
+import behaviours.ISell;
+import instruments.Guitar;
+import instruments.Material;
+import instruments.Type;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ShopTest {
 
     Shop shop;
+    Guitar guitar;
 
     @Before
     public void before() {
         shop = new Shop();
+        guitar = new Guitar(Material.WOOD, Type.STRING, 40, 80.80, 6);
+    }
+
+    @Test
+    public void canAddToStock() {
+        shop.addToStock(guitar);
+        assertEquals(1, shop.getStockCount());
     }
 }
