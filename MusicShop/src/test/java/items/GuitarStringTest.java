492438e0671f57d89820e3fbb7e623db022d1e3f
Anita Safar <ancsa0416@gmail.com>
2022-01-15 13:59:03 +0000

    hasDescription test added to GuitarStringTest

--- MusicShop/src/test/java/items/GuitarStringTest.java
+++ MusicShop/src/test/java/items/GuitarStringTest.java
@@ -1,6 +1,9 @@
 package items;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class GuitarStringTest {
 
@@ -11,6 +14,9 @@ public class GuitarStringTest {
         guitarString = new GuitarString("Yamaha", 5, 9.90);
     }
 
-
+    @Test
+    public void hasDescription() {
+        assertEquals("Yamaha", guitarString.getDescription());
+    }
 
 }
