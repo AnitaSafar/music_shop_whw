f3e90a73956c139a36e5df62fec1997d68b90ddd
Anita Safar <ancsa0416@gmail.com>
2022-01-15 14:40:39 +0000

    FlashCards class created, setup done

---
+++ MusicShop/src/main/java/items/FlashCards.java
@@ -0,0 +1,8 @@
+package items;
+
+public class FlashCards extends Item {
+
+    public FlashCards(String description, double buyingPrice, double sellingPrice) {
+        super(description, buyingPrice, sellingPrice);
+    }
+}
